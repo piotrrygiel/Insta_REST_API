d435a0b096c0116e84e996e68a4b8e22fa460347
Piotr Rygiel <64746993+piotrrygiel@users.noreply.github.com>
2023-01-13 19:55:06 +0100

    Update -01_django.sh

--- .platform/hooks/postdeploy/-01_django.sh
+++ .platform/hooks/postdeploy/-01_django.sh
@@ -2,11 +2,12 @@
 
 source /var/app/venv/*/bin/activate && {
 
-# collecting static files
+# collect static
 python manage.py collectstatic --noinput;
 # log which migrations have already been applied
 python manage.py showmigrations;
+# migrate user model
+python manage.py migrate users --noinput;
 # migrate the rest
 python manage.py migrate --noinput;
-# another command to create a superuser (write your own)
 }
